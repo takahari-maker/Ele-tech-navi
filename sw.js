6be6aa5d658709704cd29198036bf7e0f7af0cc5
takahari-maker <k-takahari@kyokujitsu.co.jp>
2026-03-29 08:24:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'maruwakari-v2';
+const CACHE_NAME = 'tech-navi-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
